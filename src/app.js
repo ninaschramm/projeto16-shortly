99574378862100896a13034f52a7af873fdac89c
ninaschramm <schrammnina@outlook.com>
2022-08-04 18:33:50 -0300

    server initial config

---
+++ src/app.js
@@ -0,0 +1,31 @@
+import express, {json} from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+const server = express();
+
+dotenv.config();
+
+server.use(
+  express.urlencoded({
+      extended: true,
+  }),
+  express.json(), cors()
+);
+
+server.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', '*');
+  res.setHeader('Access-Control-Allow-Headers', '*');
+  next();
+});
+
+
+
+//server.use(router, authRouter, prodRouter, navigationRouter);
+
+const PORT = process.env.PORT || 5000
+
+server.listen(PORT, () => {
+    console.log(`Server is listening on ${PORT}`);
+  });
