ca51bbfbd439bfc2ea00a8cbd1bbc3a35b891f92
ninaschramm <schrammnina@outlook.com>
2022-08-08 15:54:49 -0300

    heroku config

--- src/dbStrategy/postgres.js
+++ src/dbStrategy/postgres.js
@@ -5,25 +5,25 @@ dotenv.config();
 
 const { Pool } = pg;
 
-const user = 'postgres';
-const password = 'hojeedia14';
-const host = 'localhost';
-const port = 5432;
-const database = 'shortly';
-
-const connection = new Pool({
-  user,
-  password,
-  host,
-  port,
-  database
-});
+// const user = 'postgres';
+// const password = 'hojeedia14';
+// const host = 'localhost';
+// const port = 5432;
+// const database = 'shortly';
 
 // const connection = new Pool({
-//     connectionString: process.env.DATABASE_URL,
-//     ssl: {
-//         rejectUnauthorized: false
-//     }
-//   });
+//   user,
+//   password,
+//   host,
+//   port,
+//   database
+// });
+
+const connection = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+  });
 
 export default connection
